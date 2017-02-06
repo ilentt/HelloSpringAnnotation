b1cc28d53d86868d099bd988b5c6d8ea06d08e20
ilentt <ilentt@ilenlab.com>
2017-02-06 11:33:12 +0700

    update control

--- src/main/java/com/ilenlab/spring/config/AppConfiguration.java
+++ src/main/java/com/ilenlab/spring/config/AppConfiguration.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.ilenlab.spring.Language;
-import com.ilenlab.spring.lang.impl.English;
+import com.ilenlab.spring.lang.impl.Vietnamese;
 
 @Configuration
 @ComponentScan({"com.ilenlab.spring.bean"})
 public class AppConfiguration {
 	@Bean(name = "language")
 	public Language getLanguage() {
-		return new English();
+		//return new English();
+		return new Vietnamese();
 	}
 }
